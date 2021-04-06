e2ed5c9c27139c0196568b94908b41e3fd661614
Ian W. Campbell <ixc9335@rit.edu>
2021-04-06 14:31:08 -0400

    Aligned everything and it looks good!

--- src/fs.h
+++ src/fs.h
@@ -1,50 +1,52 @@
 #ifndef FS_H_
 #define FS_H_
 
-// Default size of char buffers
-#define BUF_SiZE 4096
-
 // Block size
-#define BLOCK_SIZE 4096 // What size is this? 
+#define BLOCK_SIZE 512
 
-#define MAX_FILENAME_SIZE 128
+// Default size of char buffers
+#define BUF_SIZE BLOCK_SIZE
 
-#define MAX_OPEN_FILES 128
+//#define MAX_FILENAME_SIZE 128
 
-#define MAX_DISK_SIZE (1 << 28) // 256 MB
+//#define MAX_OPEN_FILES 128
 
-#define MIN_DISK_SIZE (1 << 20) // 1 MB
+//#define MAX_DISK_SIZE (1 << 28) // 256 MB
 
-#define MAX_BLOCKS (MAX_DISK_SIZE / BLOCK_SIZE)
+//#define MIN_DISK_SIZE (1 << 20) // 1 MB
 
-#define DIRECT_POINTER_SIZE 15
+//#define MAX_BLOCKS (MAX_DISK_SIZE / BLOCK_SIZE)
+
+#define NUM_DIRECT_POINTERS 14
+
+#define INODES_PER_BLOCK 2
 
 /*
  * This structure will contain a variety of data 
  * relating to a file/directory in the FS.
  */
 struct inode {
-    // Meta Data
+    // Meta Data 16 bytes
     uint32_t id;
     uint32_t nBlocks;
     uint32_t nBytes;
     uint32_t nRefs;
 
-    // Permission information
+    // Permission information 8 bytes
     uid_t uid;
     gid_t gid; 
     uint32_t permissions: 24;
     uint32_t nodeType: 8;
 
-    // Direct Pointers - each point to a block
-    data_u direct_pointers[DIRECT_POINTER_SIZE]; // * 16 = 
+    // Lock + Padding 4 bytes
+    uint8_t lock; // 1 byte
+    uint8_t pad[3]; // 3 bytes
 
-    // Indirect Pointers
+    // Indirect Pointers 4 bytes
     block_t *extBlock; // Points to a block
 
-    // Padding + lock = 4
-    uint8_t lock;
-    uint8_t pad[3];
+    // Direct Pointers - each point to a block 
+    data_u direct_pointers[NUM_DIRECT_POINTERS]; // (14 + 2) * 16 = 256 bytes per inode
 };
 
 // Each is 16 bytes
