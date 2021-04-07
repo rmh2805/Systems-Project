dd6689f4111856685fbf3044f479aa6216a18cb0
rmh2805 <rmh2805@g.rit.edu>
2021-04-06 21:22:38 -0400

    Should spawn tests properly

--- src/userland/testShell.c
+++ src/userland/testShell.c
@@ -9,6 +9,7 @@ int32_t testShell(uint32_t arg1, uint32_t arg2) {
 
     int32_t nRead = 0;
 
+
     swrites("Test shell started\r\nTry help for a list of commands\r\n\n");
 
     while (true) {
@@ -28,13 +29,71 @@ int32_t testShell(uint32_t arg1, uint32_t arg2) {
             swrites("\r\nMain test shell help:\r\n");
             swrites("\thelp: prints this screen\r\n");
             swrites("\tlogout: return to sign in\r\n");
+            swrites("\tlist [n]: List all tests (if n is specified, all tests in that bank)\r\n");
+            swrites("\ttest n x: perform test x from bank n\r\n");
 
+            continue;
         }
 
         if(strcmp(iBuf, "exit") == 0 || strcmp(iBuf, "logoff") == 0 || 
             strcmp(iBuf, "logout") == 0 || strcmp(iBuf, "`") == 0) {
             swrites("Exiting\r\n");
             exit(0);
+
+            continue;
+        }
+
+        if(nRead < 4) {
+            swrites("Unrecognized command, try \"help\" for a list of commands\r\n");
+            continue;
+        }
+
+        if(strncmp(iBuf, "list", 4) == 0) {
+            if(nRead == 4) {
+                listTests(CHAN_SIO, 0);
+            }
+
+            char* tmp = &iBuf[4];
+            while(*tmp == ' ') tmp++;
+            listTests(CHAN_SIO, *tmp);
+        } else if(strncmp(iBuf, "test", 4) == 0) {
+            // Gather test parameters (bank and selector)
+            char* tmp = &iBuf[4];
+
+            while(*tmp == ' ' && *tmp) tmp++;
+            if(*tmp == 0) {
+                swrites("Usage: test <bank> <test>\r\n");
+                continue;
+            }
+            char ch1 = *tmp++;
+
+            while(*tmp == ' ' && *tmp) tmp++;
+            if(*tmp == 0) {
+                swrites("Usage: test <bank> <test>\r\n");
+                continue;
+            }
+
+            pid_t whom = spawnTests(ch1, *tmp);
+
+            if(whom < 0) {
+                sprint(oBuf, "SHELL: **ERROR** Failure to spawn test (spawn returned %d)\r\n", whom);
+                cwrites(oBuf);
+                swrites(&oBuf[7]);
+                continue;
+            }
+
+            sprint(oBuf, "SHELL: Spawned test %c.%c (pid %d)\r\n", 
+                (uint32_t) ch1, (uint32_t) *tmp, whom);
+            cwrites(oBuf);
+
+            status_t status;
+            whom = wait(&status);
+
+            sprint(oBuf, "SHELL: Test process (pid %d) returned status %d\r\n", whom, status);
+            cwrites(oBuf);
+
+        } else {
+            swrites("Unrecognized command, try \"help\" for a list of commands\r\n");
         }
 
     }
