a049e7ea1e9850a873a8a4ec0fe6c7e231726882
rmh2805 <rmh2805@g.rit.edu>
2021-04-13 14:20:18 -0400

    More work done on fs  image generator

--- src/MakeFSImage.c
+++ src/MakeFSImage.c
@@ -5,7 +5,9 @@
 
 #include <stdio.h>
 
-size_t diskSize, nInodes;
+#define INODES_PER_BLOCK ((int32_t)(sizeof(inode_t) / BLOCK_SIZE))
+
+uint32_t diskSize, nInodes;
 char * oFileName;
 char * pgmName;
 
@@ -13,8 +15,8 @@ void printUsage(bool_t isErr) {
     fprintf((isErr) ? stderr : stdout, "Usage: %s <output file> <disk size> <nInodes>\n", pgmName);
 }
 
-int decStr2int(char * str) {
-    int val = 0, sign = 1;
+int32_t decStr2int(char * str) {
+    uint32_t val = 0, sign = 1;
     
     if(*str == '-') {
         sign = -1;
@@ -43,10 +45,12 @@ int main(int argc, char** argv) {
     nInodes = decStr2int(argv[3]);
     
     if(diskSize == 0 || nInodes == 0 || diskSize < nInodes * sizeof(inode_t)) {
-        fprintf(stderr, "Disk size (%lu bytes) not enough to store %lu inodes (need at least %lu bytes)\n", diskSize, nInodes, nInodes * sizeof(inode_t));
+        fprintf(stderr, "Disk size (%u bytes) not enough to store %u inodes (need at least %u bytes)\n", 
+                diskSize, nInodes, nInodes * sizeof(inode_t));
         return -1;
     }
-    
+
+    printf("inode blocks: %u\n", INODES_PER_BLOCK);
     
     return 0;
 }
