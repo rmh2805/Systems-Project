eaf43e5d6efba62e731b49b4c8320e1d97621d76
rmh2805 <rmh2805@g.rit.edu>
2021-04-20 15:28:17 -0400

    Final minor changes before signing off

--- src/ramDiskDriver.c
+++ src/ramDiskDriver.c
@@ -2,5 +2,12 @@
 
 void _rd_init();
 
-int _rd_readBlock(uint32_t blockNr, char* buf, uint8_t devId);
-int _rd_writeBlock(uint32_t blockNr, char* buf, uint8_t devId);
+int _rd_readBlock(uint32_t blockNr, char* buf, uint8_t devId) {
+
+    return E_FAILURE;
+}
+
+int _rd_writeBlock(uint32_t blockNr, char* buf, uint8_t devId) {
+
+    return E_FAILURE;
+}
