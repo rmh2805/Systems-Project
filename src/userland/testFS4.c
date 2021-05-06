00d20d534ba71bf2f235dee872ac41e4fcf68dd2
Ian W. Campbell <ixc9335@rit.edu>
2021-05-06 13:31:38 -0400

    Added test file

---
+++ src/userland/testFS4.c
@@ -0,0 +1,21 @@
+#ifndef TEST_FS_4_H_
+#define TEST_FS_4_H_
+
+#include "common.h"
+
+int testFS4(uint32_t arg1, uint32_t arg2) {
+    char buf[100];
+    int ret;
+
+    ret = fremove("/", "test.txt");
+    if(ret < 0) {
+        sprint(buf, "Test FS %d.%d: Failed to remove \"/test.txt\" (exit status %d)\r\n", arg1, arg2, ret);
+        swrites(buf);
+        return ret;
+            
+    }
+
+    return 0;
+}
+
+#endif
